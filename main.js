80b6df5b2617a6f6cb6c87b8e31b3034f08b6cc3
Rajesh Patro <rajeshpatra096@gmail.com>
2025-06-19 22:43:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+const list = document.querySelector(".navlist");
+const hamburger = document.querySelector(".fa-bars");
+
+hamburger.addEventListener("click", () => {
+
+  hamburger.classList.toggle("fa-x");
+  list.classList.toggle("navlist-active");
+
+});
